e0ef9b458d48ae685d68caf1759bbc718f83c4f0
Huzaifa Juzer <huzaifajuzer@hotmail.com>
2022-02-17 11:02:57 +0500

    Working with JSON

--- app/views.py
+++ app/views.py
@@ -1,5 +1,24 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, jsonify, make_response
+
+users = {
+    "mitsuhiko": {
+        "name": "Armin Ronacher",
+        "bio": "Creator of of the Flask framework",
+        "twitter_handle": "@mitsuhiko"
+    },
+    "gvanrossum": {
+        "name": "Guido Van Rossum",
+        "bio": "Creator of the Python programming language",
+        "twitter_handle": "@gvanrossum"
+    },
+    "elonmusk": {
+        "name": "Elon Musk",
+        "bio": "technology entrepreneur, investor, and engineer",
+        "twitter_handle": "@elonmusk"
+    }
+}
+
 
 @app.route('/')
 def index():
@@ -8,3 +27,64 @@ def index():
 @app.route('/about')
 def about():
     return render_template('public/about.html')
+
+@app.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+
+    if request.method == "POST":
+
+        # username = request.form.get("username")
+        # email = request.form.get("email")
+        # password = request.form.get("password")
+
+        # Alternatively
+
+        # username = request.form["username"]
+        # email = request.form["email"]
+        # password = request.form["password"]
+
+        req = request.form
+
+        missing = list()
+
+        for k, v in req.items():
+            if v == "":
+                missing.append(k)
+
+        if missing:
+            feedback = f"Missing fields for {', '.join(missing)}"
+            return render_template("public/sign_up.html", feedback=feedback)
+
+        return redirect(request.url)
+        
+    return render_template("public/sign_up.html")
+
+@app.route("/profile/<username>")
+def profile(username):
+
+    user = None
+
+    if username in users:
+        user = users[username]
+
+    return render_template("public/profile.html", username=username, user=user)
+
+@app.route("/json", methods=["POST"])
+def json_example():
+
+    if request.is_json:
+
+        req = request.get_json()
+
+        response_body = {
+            "message": "JSON received!",
+            "sender": req.get("name")
+        }
+
+        res = make_response(jsonify(response_body), 200)
+
+        return res
+
+    else:
+
+        return make_response(jsonify({"message": "Request body must be JSON"}), 400)
